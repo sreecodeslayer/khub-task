ff997ff71a03569ca9c6be86998f8307725f66a3
Sreenadh TC <kesav.tc8@gmail.com>
2018-04-12 15:48:40 +0530

    Introduce database into api, setup params for GET Crops

--- agmapi/__init__.py
+++ agmapi/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+from .db import init_db
 from flask_restful import Api
 
 from .web import CropsResource
 
 app = Flask('agmapi')
+app.config['DEBUG'] = True
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'AGMAPI',
+    'host': 'mongodb://localhost:27017/AGMAPI'
+}
+
+db = init_db(app)
 
 app.route('/')
 
--- agmapi/web/crops.py
+++ agmapi/web/crops.py
@@ -1,7 +1,23 @@
 from flask_restful import Resource
+from flask import (
+    request,
+    jsonify,
+    make_response
+)
 
 
 class CropsResource(Resource):
 
     def get(self):
+        # Accept and parse params
+        params = request.args
+
+        date = params.get('date')
+        frm = params.get('from')
+        to = params.get('to')
+        crop_name = params.get('crop')
+        mandi_name = params.get('mandi')
+        page = params.get('page')
+        per_page = params.get('perPage')
+
         pass
